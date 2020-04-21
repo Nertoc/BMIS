157f3b2c456be3525441635f50bf915c283f2fe0
Nertoc <62835673+Nertoc@users.noreply.github.com>
2020-04-21 19:09:14 +1000

    Router Work using Vue
    Realised a Key work issue with Vue - App loads routers, router is stored under views - views are our different webpages.

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <TopHeader />
+  </div>
+</template>
+
+<script>
+import TopHeader from "@/components/TopHeader.vue";
+export default {
+  components: {
+    TopHeader
+  }
+};
+</script>
---
+++ src/views/home.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="home">
+    <div class="bgImage">
+   <TopHeader />
+      <titlePage />
+    </div>
+    <div>
+      <upcomingEvents />
+    </div>
+    <div class="catImg">
+      <eventCategories />
+    </div>
+    <div>
+      <manageEvents />
+    </div>
+    <div>
+      <bmisFooter />
+    </div>
+    <!-- add section above the green inside div containers -->
+  </div>
+</template>
+
+<script>
+import TopHeader from "@/components/TopHeader.vue";
+import titlePage from "@/components/TitlePage.vue";
+import upcomingEvents from "@/components/UpcomingEvents.vue";
+import eventCategories from "@/components/eventCategories.vue";
+import manageEvents from "@/components/EventManagement.vue";
+import bmisFooter from "@/components/BMISFooter.vue";
+
+export default {
+  name: "home",
+  components: {
+    TopHeader,
+    titlePage,
+    upcomingEvents,
+    eventCategories,
+    manageEvents,
+    bmisFooter
+  }
+};
+</script>
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+HK:wght@100;300;400;500;700;900&display=swap");
+#app {
+  font-family: "Noto Sans HK", "sans-serif";
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.bgImage {
+  background-image: url("https://i.imgur.com/etg5hVx.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  object-fit: cover;
+  height: 100%;
+}
+.catImg {
+  background-image: url("https://i.imgur.com/etg5hVx.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  object-fit: cover;
+  height: 100%;
+}
+
+#nav {
+  padding: 50px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
